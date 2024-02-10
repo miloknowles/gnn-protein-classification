4cccb9cded630be3b1c6c0f947474ea1f102b663
Milo Knowles <miloknowles97@gmail.com>
2024-02-10 10:43:11 -0500

    64

---
+++ scripts/train_cnn_v64.sh
@@ -0,0 +1,10 @@
+python train_cnn.py --train \
+  --model-id cnn_test_v64 \
+  --train-path ../data/cleaned_skip_missing/train \
+  --val-path ../data/cleaned_skip_missing/val \
+  --test-path ../data/cleaned_skip_missing/test \
+  --num-workers 4 \
+  --lr 1e-4 \
+  --voxel-grid-dim 64 \
+  --batch-size 4 \
+  --random-rotation \
